4927983e112b6182cf98267dd1cdbeb3114c9873
slotThe <soliditsallgood@mailbox.org>
2022-02-26 10:21:02 +0100

    Interpreter: Fix and document substitute
    With b000c2900ccfb2bedef4abac6fd99f56d5b9007a we now need to be careful
    how deep we traverse the structure when substituting a variable.
    
    This function is a bit of a mess, and we really should be using De
    Bruijn indices, but that's a TODO for a later day.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -25,17 +25,24 @@ eval env = go
       Nothing -> throw $ OperationNotFound op
     e -> e
 
--- | Substitute the value of a variable into another expression.
+-- | Substitute the value of a variable _in the body_ of the lambda that
+-- brought it into scope.
+--
+-- NOTE: This means that, in particular, if our variable is named "a"
+-- and we encounter a lambda that also brings a variable named "a" into
+-- scope, we immediately exit with the lambda as our final expression
+-- and do not substitute any value.  This is essentially a hack to get
+-- around having to use De Bruijn indices.
 subst :: Var -> DExpr -> DExpr -> DExpr
 subst var val = go
  where
   go :: DExpr -> DExpr
   go = \case
-    v@(DEVar v') -> if var == v' then val     else v
-    DELam v body -> if var == v  then go body else DELam v (go body)
-    DEApp f x    -> DEApp (go f) (go x)
-    DEBin op l r -> DEBin op (go l) (go r)
-    e            -> e
+    v@(DEVar v')     -> if var == v' then val else v
+    l@(DELam v body) -> if var == v  then l   else DELam v (go body)
+    DEApp f x        -> DEApp (go f) (go x)
+    DEBin op l r     -> DEBin op (go l) (go r)
+    e                -> e
 
 -- | Desugar an expression.
 desugar :: Expr -> DExpr
